530c13400ef6dea0fbc36a631cc2c4af5b8ad32d
Aayush1911 <21it49@svitvasad.ac.in>
2024-04-11 19:11:40 +0530

    Basics of python

---
+++ LEC1/firstprogram.py
@@ -0,0 +1,94 @@
+# print("Hello World")
+# print("Aayush","patel")
+# print(25)
+# name="patel"
+# age=21
+# print("Surname :",name)
+# print("age :",age)
+# print(type(name)) 
+#<class 'str'>
+# print(type(age)) 
+#<class 'int'>
+
+# python has following Datatypes:integer,string,float,boolean,none.
+# name1="hello"
+# name2='hello'
+# name3='''hello'''
+# print(name1,name2,name3)
+
+# old=G=False
+# print(type(old))
+# <class 'bool'>
+# a=None
+# print(type(a))
+# <class 'NoneType'>
+
+# sum=5+9
+# print(sum)
+
+# single line comment
+'''multi
+ line
+ comment'''
+
+#operators
+# 1 Arithmetic Operator
+# a=4
+# b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b) #power
+
+# 2 Relational Operator
+# a=2
+# b=4
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+
+# 3 Assignment Operator
+# +=
+# -=
+# *=
+# /=
+#%=
+#**=
+
+# 4 Logical Operator
+# print(not True)
+# a=True
+# b=False
+# print(a and b) #False
+# print(a or b) #True
+
+#Type Conversion
+# it has two types
+#1 Conversion(Automatically python does)
+#2 Casting(Manual)
+
+# Type 1
+# a=2
+# b=2.3
+# sum=a+b #2.0+2.3=4.3
+# print(sum)
+
+# Type 2
+# a=int('2')
+# b=2
+# print(type(a))
+# print(a+b)
+
+# input from user
+# name=input("Enter your name: ")
+# # result of input() is always a str
+# print("Welcome",name)
+# print(type(name))
+
+# type cast
+# num=int(input("enter num : "))
+# print(type(num))
+
