234be44120ca63b2e6d6a1bb6cec1e2eb2348c3b
Aayush1911 <21it49@svitvasad.ac.in>
2024-04-11 20:24:37 +0530

    Learn String and Conditional Statements

---
+++ LEC2/conditionalstatements.py
@@ -0,0 +1,16 @@
+# age=17
+# if(age>=18):
+#     print('You can vote')
+# elif(age==17):
+#     print('You are one year away to vote')
+# else:
+#     print('You can not vote')
+
+age=19
+if(age>=18):
+    if(age>=80):
+        print("Cannot drive because age more")
+    else:
+        print("Can drive")
+else:
+    print('Can not drive age is less')
---
+++ LEC2/strings.py
@@ -0,0 +1,53 @@
+# str1="Aayush\nPatel"
+# str2='Aayush Patel'
+# str3='''Aayush Patel'''
+# print(str1,str2,str3)
+
+# concat
+# str1="Aayush"
+# str2="Patel"
+# print(str1+" "+str2)
+
+# length of string function
+# str1="Aayush"
+# print(len(str1))
+
+# indexing
+# it starts with 0
+# str1='Aayush'
+# print(str1[2])
+
+# slicing
+# Accessing parts of string
+# first index is exclusive
+# second index is not exclusive 
+# str1="Aayush"
+# print(str1[1:3]) #ay
+# print(str1[2:]) #yush
+# str1[:2] is same as [0:2]
+# str1[1:] is same as str1[1:len(str1)]
+
+# Negative index
+#  a p p l e
+# -5-4-3-2-1 
+# str='Apple'
+# print(str[-4:-1]) #ppl
+
+# string functions
+str='i am a coder'
+
+# print(str.endswith('er'))
+
+# print(str.capitalize()) #only first letter gets capital. This creates a new string not updates the string.
+# to update the string
+# str=str.capitalize()
+# print(str)
+
+# print(str.replace('o','a')) #This creates a new string not updates the string.
+
+# print(str.find('a'))
+
+# print(str.count('a'))
+
+
+
