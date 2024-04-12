181c06e89d58abb9456bf06fecd5b76f4d765be7
Aayush1911 <21it49@svitvasad.ac.in>
2024-04-12 14:01:47 +0530

    Learn function and recursion in python

---
+++ LEC6/function.py
@@ -0,0 +1,30 @@
+# def sum(a,b): #function definition,parameter
+#     s=a+b
+#     return s
+
+# print(sum(5,5))#function call,argument
+
+# Print list in single line
+# list=[1,2,3,4,5,6]
+
+# def print_list(list):
+#     for i in list:
+#         print(i,end=' ')
+
+# print_list(list)
+
+# find factorial
+# def fact(n):
+#     f=1
+#     for i in range(1,n+1):
+#         f*=i
+#     print(f)
+
+# fact(5)
+
+# convert usd to inr
+# def converter(usd):
+#     inr=83*usd
+#     print(usd,"USD =",inr,"INR")
+
+# converter(5)
